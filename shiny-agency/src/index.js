a1bd12864be3163ccd86ec33b07904fbba925669
NDOH NDOH Jules Moise Yannick <ndohmoise@gmail.com>
2023-07-11 14:22:33 +0200

    mise à jour du projet

--- shiny-agency/src/index.js
+++ shiny-agency/src/index.js
@@ -1,13 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import reportWebVitals from './reportWebVitals';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Survey from './pages/Survey'
 import Results from './pages/Results'
 import Freelances from './pages/Freelances'
 import Header from './components/Header'
 import Error from './components/Error'
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  * {
+      font-family: 'Trebuchet MS', Helvetica, sans-serif;
+    }
+
+  body {
+      margin: 0;
+    }
+`
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -15,11 +25,11 @@ root.render(
     <Router>
       <Header />
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/survey/:uestionNumber' element={<Survey />} />
-        <Route path='/results' element={<Results />} />
-        <Route path='/freelances' element={<Freelances />} />
-        <Route path='*' element={<Error />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/survey/:questionNumber" element={<Survey />} />
+        <Route path="/results" element={<Results />} />
+        <Route path="/freelances" element={<Freelances />} />
+        <Route path="*" element={<Error />} />
       </Routes>
     </Router>
   </React.StrictMode>
@@ -28,4 +38,3 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
--- shiny-agency/src/pages/Freelances/index.jsx
+++ shiny-agency/src/pages/Freelances/index.jsx
@@ -1,9 +1,64 @@
+import Card from '../../components/Card'
+import styled from 'styled-components'
+import colors from '../../utils/style/colors'
+
+const CardsContainer = styled.div`
+  display: grid;
+  gap: 24px;
+  grid-template-rows: 350px 350px;
+  grid-template-columns: repeat(2, 1fr);
+  align-items: center;
+  justify-items: center;
+`
+
+const PageTitle = styled.h1`
+  font-size: 30px;
+  color: black;
+  text-align: center;
+  padding-bottom: 30px;
+`
+
+const PageSubtitle = styled.h2`
+  font-size: 20px;
+  color: ${colors.secondary};
+  font-weight: 300;
+  text-align: center;
+  padding-bottom: 30px;
+`
+
+const freelanceProfiles = [
+  {
+    name: 'Jane Doe',
+    jobTitle: 'Devops',
+  },
+  {
+    name: 'John Doe',
+    jobTitle: 'Developpeur frontend',
+  },
+  {
+    name: 'Jeanne Biche',
+    jobTitle: 'Développeuse Fullstack',
+  },
+]
+
 function Freelances() {
-    return (
-      <div>
-        <h1>Freelances 👩‍💻👨‍💻👩‍💻</h1>
-      </div>
-    )
-  }
-  
-  export default Freelances
+  return (
+    <div>
+      <PageTitle>Trouvez votre prestataire</PageTitle>
+      <PageSubtitle>
+        Chez Shiny nous réunissons les meilleurs profils pour vous.
+      </PageSubtitle>
+      <CardsContainer>
+        {freelanceProfiles.map((profile, index) => (
+          <Card
+            key={`${profile.name}-${index}`}
+            label={profile.jobTitle}
+            title={profile.name}
+          />
+        ))}
+      </CardsContainer>
+    </div>
+  )
+}
+
+export default Freelances
